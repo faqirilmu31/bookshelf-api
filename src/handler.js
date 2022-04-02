680071f9df899aa156fc83de1d48b7eb1003768a
Just an ordinary girl <renisetyaningsih2931@gmail.com>
2022-04-03 05:05:32 +0700

    config eslint

---
+++ src/handler.js
@@ -0,0 +1,10 @@
+const addBookHandler = (request, h) => {
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading 
+  } = request.payload;
+  
+};
+
+module.exports = {
+  addBookHandler,
+}
